52ebb9e44f60033e21e685bc199a28039d448a3f
4nth0ny1 <acatullo4@gmail.com>
2023-08-10 23:34:54 -0500

    added ui for AccountProfile form

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,10 +3,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
-import Topbar from "../components/shared/Topbar";
-import LeftSidebar from "../components/shared/LeftSidebar";
-import RightSidebar from "../components/shared/RightSidebar";
-import Bottombar from "../components/shared/Bottombar";
+import Topbar from "../../components/shared/Topbar";
+import LeftSidebar from "../../components/shared/LeftSidebar";
+import RightSidebar from "../../components/shared/RightSidebar";
+import Bottombar from "../../components/shared/Bottombar";
 
 const inter = Inter({ subsets: ["latin"] });
 
